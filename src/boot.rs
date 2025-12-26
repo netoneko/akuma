b85439dece8abd501e2a30fd0978cd2882e4a4e6
Kirill Maksimov <netoneko@gmail.com>
2025-12-26 21:25:46 +0800

    fix boot.rs

--- src/boot.rs
+++ src/boot.rs
@@ -9,6 +9,12 @@ global_asm!(
     ".global _boot",
     "_boot:",
     "    mov x19, x0",            // Save DTB pointer from x0 to x19
+
+    // Enable FPU/SIMD (CPACR_EL1: set FPEN bits 20-21 to 0b11)
+    "    mov x0, #(3 << 20)",     // FPEN = 0b11: no trapping of FP/SIMD
+    "    msr cpacr_el1, x0",      // Write to CPACR_EL1
+    "    isb",                    // Instruction barrier
+
     "    ldr x1, =DTB_FROM_BOOT", // Load address of global variable
     "    str x19, [x1]",          // Store x0 value to prove boot ran
     "    ldr x0, =0x40100000",    // Load stack address
