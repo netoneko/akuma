8efb342ec37ea474808bf2a5362747804f82f65e
Kirill Maksimov <netoneko@gmail.com>
2026-01-29 21:32:16 +0200

    meow client

--- userspace/build.sh
+++ userspace/build.sh
@@ -12,8 +12,9 @@ cp target/aarch64-unknown-none/release/stackstress ../bootstrap/bin/
 cp target/aarch64-unknown-none/release/stdcheck ../bootstrap/bin/
 cp target/aarch64-unknown-none/release/elftest ../bootstrap/bin/
 cp target/aarch64-unknown-none/release/httpd ../bootstrap/bin/
+cp target/aarch64-unknown-none/release/meow ../bootstrap/bin/
 cp target/aarch64-unknown-none/release/wget ../bootstrap/bin/
 cp target/aarch64-unknown-none/release/sqld ../bootstrap/bin/
 cp target/aarch64-unknown-none/release/qjs ../bootstrap/bin/
 
-echo "Built and copied: hello, echo2, stackstress, stdcheck, elftest, httpd, wget, sqld, qjs"
+echo "Built and copied: hello, echo2, stackstress, stdcheck, elftest, httpd, meow, wget, sqld, qjs"
