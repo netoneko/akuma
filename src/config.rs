51e499f5b2c13a552a265e77c89136ce7bc78990
Kirill Maksimov <netoneko@gmail.com>
2026-02-19 21:21:26 +0200

    hell yeah

--- src/config.rs
+++ src/config.rs
@@ -195,7 +195,7 @@ pub const ENABLE_SSH_ASYNC_EXEC: bool = true;
 pub const STDOUT_TO_KERNEL_LOG_COPY_ENABLED: bool = true;
 
 /// Option to disable [syscall] debug prints to the kernel log.
-pub const SYSCALL_DEBUG_INFO_ENABLED: bool = true;
+pub const SYSCALL_DEBUG_INFO_ENABLED: bool = false;
 
 /// Option to disable [ext2] debug prints to the kernel log.
 pub const DEBUG_EXT2: bool = false;
